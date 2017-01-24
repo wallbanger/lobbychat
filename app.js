63eac1a1a1a0cc63f57e73625ef9ca51ad83e443
Eugene Koval <iam.eugenekoval@gmail.com>
2017-01-24 22:36:54 +0200

    update app and create start page template

--- app.js
+++ app.js
@@ -2,41 +2,34 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var config = require('./config');
+var log = require('./libs/log')(module);
 
 var app = express();
-app.set('port', config.get('port'));
 
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + config.get('port'));
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');
+
+app.use(express.favicon());
+
+if(app.get('env') == 'development') {
+    app.use(express.logger('dev'));
+} else {
+    app.use(express.logger('default'));
+}
+
+app.use(express.bodyParser());
+
+app.use(express.cookieParser());
+
+app.use(app.router);
+
+app.get('/', function (req, res, next) {
+    res.render('index', {
+        body: '<b>hello</b>'
+    });
 });
 
-app.use(function (req, res, next) {
-    if(req.url == '/') {
-        res.end('Hello');
-    } else {
-        next();
-    }
-});
-
-app.use(function (req, res, next) {
-    if(req.url == '/forbidden') {
-        next(new Error('whoops, denied'));
-    } else {
-        next();
-    }
-});
-
-app.use(function (req, res, next) {
-    if(req.url == '/page') {
-        res.end('Page');
-    } else {
-        next();
-    }
-});
-
-app.use(function (req, res) {
-   res.send(404, 'Woops, Page not found!')
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (err, req, res, next) {
     // NODE_ENV == 'production'
@@ -52,17 +45,10 @@ app.use(function (err, req, res, next) {
 // var user = require('./routes/user');
 //
 // // all environments
-// app.set('port', process.env.PORT || 3000);
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-// app.use(express.favicon());
-// app.use(express.logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded());
-// app.use(express.methodOverride());
-// app.use(express.session({ secret: 'your secret here' }));
-// app.use(app.router);
-// app.use(express.static(path.join(__dirname, 'public')));
 //
 // app.get('/', routes.index);
 // app.get('/users', user.list);
+
+http.createServer(app).listen(config.get('port'), function(){
+    log.info('Express server listening on port ' + config.get('port'));
+});
