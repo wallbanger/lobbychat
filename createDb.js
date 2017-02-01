f90092adfe3875fc3f4e27a0c2a58dccff414669
Eugene Koval <iam.eugenekoval@gmail.com>
2017-02-01 22:55:14 +0200

    update db connection

--- createDb.js
+++ createDb.js
@@ -5,15 +5,18 @@ MongoClient.connect('mongodb://127.0.0.1:27017/chat', function (err, db) {
     if(err) throw err;
 
     var collection = db.collection('test_insert');
-    collection.insert({a:2}, function (err, docs) {
 
-        collection.count(function (err, count) {
-            console.log(format('count = %s', count));
-        });
+    collection.remove({}, function (err, affected) {
+        if(err) throw err;
 
-        collection.find().toArray(function (err, results) {
-            console.dir(results);
-            db.close();
+        collection.insert({a: 2}, function (err, docs) {
+
+            var cursor = collection.find({a: 2});
+
+            cursor.toArray(function (err, results) {
+                console.dir(results);
+                db.close();
+            });
         });
     });
 });
