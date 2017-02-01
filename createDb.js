6dcbcb84ade34c932604784971e1552048568a82
Eugene Koval <iam.eugenekoval@gmail.com>
2017-02-01 22:42:25 +0200

    upd

--- createDb.js
+++ createDb.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient,
     format = require('util').format;
 
-MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db) {
+MongoClient.connect('mongodb://127.0.0.1:27017/chat', function (err, db) {
     if(err) throw err;
 
     var collection = db.collection('test_insert');
