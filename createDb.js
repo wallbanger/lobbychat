5e8c4c6a8fcd663dca94b52b02069ac35b77fb5b
Eugene Koval <iam.eugenekoval@gmail.com>
2017-02-01 22:36:05 +0200

    create db

---
+++ createDb.js
@@ -0,0 +1,19 @@
+var MongoClient = require('mongodb').MongoClient,
+    format = require('util').format;
+
+MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db) {
+    if(err) throw err;
+
+    var collection = db.collection('test_insert');
+    collection.insert({a:2}, function (err, docs) {
+
+        collection.count(function (err, count) {
+            console.log(format('count = %s', count));
+        });
+
+        collection.find().toArray(function (err, results) {
+            console.dir(results);
+            db.close();
+        });
+    });
+});
